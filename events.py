8f35bd02d16e7d40db0189d1e38396f93a06caaf
Ray <ray040123@gmail.com>
2013-03-18 22:09:36 +0800

    Add new events. Implement bind/unbind/emit in EventManager

--- events.py
+++ events.py
@@ -1,8 +1,12 @@
 # types of events
+DEFAULT_EVENT = 'default-event'
+GAME_EVENT = 'game-event'
 SNAKE_DIE = "snake-die"
 SNAKE_EAT = "snake-eat"
 SNAKE_MOVE = "snake-move"
 HIGH_SCORE = "high-score"
+FOOD_GEN = "food-gen"
+FOOD_DISAPPEAR = "food-disappear"
 # etc ..
 
 # mapping
@@ -16,15 +20,20 @@ def bound(evt, callback):
     pass
 
 class Event(object):
-    def __init__(self, target, evt_type):
-        this.target = target
-        this.evt_type = evt_types
-
-class GameEvent(Event):
-    def __init__(self, type, *args, **kwargs):
-        self.type = type
+    type = DEFAULT_EVENT
+    def __init__(self, *args, **kwargs):
         for key, val in kwargs.iteritems():
             setattr(self, key, val)
+
+    def __repr__(self):
+        return '{self.__class__.__name__}(type={self.type!r}, {desc})'.format(
+                self=self, 
+                desc=', '.join('{}={!r}'.format(k, v) 
+                    for (k, v) in self.__dict__.iteritems() if k != 'type'),
+                )
+
+class GameEvent(Event):
+    pass
 
 # maybe subclassing will be better
 class SnakeDie(GameEvent):
@@ -41,3 +50,87 @@ class SnakeEat(GameEvent):
         self.pos = snake.headPos
         self.food = food
         self.snake = snake
+
+class SnakeMove(GameEvent):
+    type = SNAKE_MOVE
+    def __init__(self, snake):
+        self.snake = snake
+
+class FoodGen(GameEvent):
+    type = FOOD_GEN
+    def __init__(self, food, pos):
+        self.food = food
+        self.pos = pos
+
+class FoodDisappear(GameEvent):
+    type = FOOD_DISAPPEAR
+    def __init__(self, food, pos):
+        self.food = food
+        self.pos = pos
+
+class EventManager:
+    def __init__(self):
+        self._eventHandlers = {}
+
+    def bind(self, eventType, handler):
+        if eventType not in self._eventHandlers:
+            self._eventHandlers[eventType] = set()
+        self._eventHandlers[eventType].add(handler)
+
+    def unbind(self, eventType, handler):
+        self._eventHandlers[eventType].remove(handler)
+
+    def emit(self, event):
+        eventType = event.type
+        for handler in self._eventHandlers[eventType]:
+            handler(event)
+
+    # @staticmethod
+    # def test():
+    #     menuManager = EventManager()
+    #     menuManager.bind(KeyDownEvent('q'), game.quit)
+    #     menuManager.bind(KeyDownEvent('up'), menu.select_previous)
+    #     menuManager.bind(KeyDownEvent('down'), menu.select_next)
+    #     def finish(event):
+    #         game.eventManager = gamingManager
+    #     menuManager.bind(KeyDownEvent('enter'), finish)
+
+    #     gamingManager = EventManager()
+    #     gamingManager.bind(KeyDownEvent('up'), lambda e: player1.up)
+    #     gamingManager.bind(KeyDownEvent('down'), lambda e: player1.down)
+
+    #     game.eventManager = menuManager
+    #     game.main_loop()
+
+if __name__ == '__main__':
+    def barker(msg):
+        def func(event):
+            print 'barker:', msg, event
+        return func
+
+    def test_bind():
+        print '=' * 80
+        mgr = EventManager()
+        mgr.bind(GameEvent.type, barker("game event!"))
+
+    def test_emit():
+        print '=' * 80
+        mgr = EventManager()
+        mgr.bind(GameEvent.type, barker("game event!"))
+        print 'emit'
+        mgr.emit(GameEvent(foo="bar"))
+
+    def test_unbind():
+        print '=' * 80
+        mgr = EventManager()
+        handler = barker("game event!")
+        mgr.bind(GameEvent.type, handler)
+        print 'emit'
+        mgr.emit(GameEvent(foo="bar"))
+        mgr.unbind(GameEvent.type, handler)
+        print 'emit'
+        mgr.emit(GameEvent(foo="bar"))
+
+    test_bind()
+    test_emit()
+    test_unbind()

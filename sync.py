7441cf8f82d78ff028af055400943224dca55670
Ray <ray040123@gmail.com>
2013-03-20 00:44:46 +0800

    Use the sync methods to update world.

--- sync.py
+++ sync.py
@@ -1,16 +1,45 @@
 # Author:
 # Description:
+from debug import dprint
 
-# import blablab
-
-class Lock(cocos.sprite.Sprite): # better alternative?
+class Lock:
     def __init__(self):
-        self.sema = 0
-        self.waiting_list = []
+        self.waitingList = []
         self.owner = None
 
-    def aquire(self, target):
-        pass
+    def __repr__(self):
+        return "Lock(pos={}, owner={}, waiting={})".format(self.pos, self.owner, len(self.waitingList))
 
-    def release(self, target)
-        pass
+    def acquire(self, target, on_succeed, on_fail):
+        dprint("{} acquire for {}".format(target, self))
+        self.waitingList.append((target, on_succeed, on_fail))
+
+    def release(self, target):
+        dprint("{} release {}".format(target, self))
+        if self.owner != target:
+            raise Exception("{} is not holding the lock. Onwer: {}".format(target, self.owner))
+        self.owner = None
+
+    def update(self):
+        """
+        @return: True if something updated. False if still blocking.
+        """
+        if len(self.waitingList) > 1:
+            self.fail()
+            return True
+        if self.owner is None:
+            if len(self.waitingList) == 1:
+                target, on_succeed, on_fail = self.waitingList.pop()
+                dprint('give', self, 'to', target)
+                if on_succeed:
+                    on_succeed()
+                self.owner = target
+                return True
+        return False
+
+    def fail(self):
+        dprint(self, 'failed')
+        for target, on_succeed, on_fail in self.waitingList:
+            if on_fail:
+                on_fail()
+        self.waitingList = []
--- world.py
+++ world.py
@@ -25,83 +25,23 @@ class World:
             player.update()
 
         field = self.field
-        racing = {}
-        # block all snakes that racing for a same position or hitting walls
-        nextPositions = [(snake, snake.next_positions()) for snake in self.snakes]
-        for snake, ps in nextPositions:
-            nextPos = ps[0]
-            grid = field.get_grid_at(*nextPos)
-            if grid:
-                racing[nextPos] = racing.get(nextPos, 0) + 1
-            else:
-                snake.blocked = True
-        for snake, ps in nextPositions:
-            if not snake.blocked and racing[ps[0]] > 1:
-                snake.blocked = True
+        for snake in self.snakes:
+            snake.update(eventMgr)
+
         isStatic = False
         while not isStatic:
             isStatic = True
-            # block all snakes that will hit other snakes
-            nextPositions = [(snake, snake.next_positions()) for snake in self.snakes]
-            for snake, ps in nextPositions:
-                nextPos = ps[0]
-                if nextPos in ps[1:]:
-                    dprint('{} block by {}'.format(snake, 'self'), 'headPos', headPos, 'poss[1:]', poss[1:])
-                    snake.blocked = True
+            blockingLocks = []
+            for grid in self.field:
+                lock = grid.lock
+                if lock.update():
                     isStatic = False
-                    break
-                for snake1, ps1 in nextPositions:
-                    if snake1 is not snake and nextPos in ps1:
-                        dprint('{} block by {}'.format(snake, snake1))
-                        snake.blocked = True
-                        isStatic = False
-                        break
-                if not isStatic: break
-        # handle collisions
-        for snake in self.snakes:
-            if snake.blocked:
-                nextHead = snake.next_head_pos()
-                grid = field.get_grid_at(*nextHead)
-                if grid and grid.type == grids.FOOD:
-                    eatCount = eatings[grid.position]
-                    if eatCount > 1:
-                        # the snake will die, since it race for food with other
-                        # snake
-                        eventMgr.emit(SnakeDie(
-                            reason="racing for food",
-                            snake=snake,
-                            pos=nextHead,
-                            ))
-                        snake.die()
-                    else:
-                        # the snake cat eat the food
-                        snake.blocked = False
-                        snake.update()
-                        eventMgr.emit(SnakeMove(snake))
-                        eventMgr.emit(FoodDisappear(
-                            food=grid.content, 
-                            pos=grid.position,
-                            ))
-                elif grid:
-                    # the snake will die, since it's blocked by thing other 
-                    # than food
-                    if grid.type == grids.SNAKE:
-                        reason = "blocked by other snake"
-                    elif grid.type == grids.BLANK:
-                        reason = "race for a position"
-                    eventMgr.emit(SnakeDie(reason=reason, snake=snake, pos=nextHead))
-                    snake.die()
-                elif grid is None:
-                    eventMgr.emit(SnakeDie(
-                        reason="blocked by field border",
-                        snake=snake,
-                        pos=nextHead,
-                        ))
-                    snake.die()
-            else:
-                # the snake can move
-                snake.update()
-                eventMgr.emit(SnakeMove(snake))
+                elif lock.waitingList:
+                    blockingLocks.append(lock)
+        #TODO: handle the circle situation
+        for lock in blockingLocks:
+            lock.fail()
+
         self.snakes = [snake for snake in self.snakes if snake.alive]
 
 if __name__ == '__main__':
